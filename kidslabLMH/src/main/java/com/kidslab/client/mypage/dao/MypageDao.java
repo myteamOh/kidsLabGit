5ff3118b060500302e9be9e0bbc020d3034512a2
GeeHun Oh <ohgeehun90@naver.com>
2018-04-02 16:52:57 +0900

    Delete

--- kidslabLMH/src/main/java/com/kidslab/client/mypage/dao/MypageDao.java
+++
@@ -1,9 +0,0 @@
-package com.kidslab.client.mypage.dao;
-
-import com.kidslab.client.parent.vo.ParentVO;
-
-public interface MypageDao {
-
-	public ParentVO parentInfo(int parentNum);
-
-}
--- kidslabLMH/src/main/java/com/kidslab/client/mypage/service/MypageService.java
+++
@@ -1,9 +0,0 @@
-package com.kidslab.client.mypage.service;
-
-import com.kidslab.client.parent.vo.ParentVO;
-
-public interface MypageService {
-
-	public ParentVO parentModify(int parentNum);
-
-}
